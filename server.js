0361a2ce099ea32191dd73515cdd02bcb53bc198
D BOSS <divasregmi@gmail.com>
2020-08-03 14:43:26 +0545

    Add validation for profile and implement  routes for post

--- server.js
+++ server.js
@@ -1,44 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const bodyParser = require('body-parser');
-const passport = require('passport')
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 //API's Routes
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 //use bodyParser middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Db configuration
 const db = require("./confg/key").mongoURI;
 
 //connect to db
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("mongoose conected successfully"))
   .catch((err) => console.log(err));
 
+//password middleware
+app.use(passport.initialize());
 
-  //password middleware
-  app.use(passport.initialize())
+//passport configuration
 
-
-  //passport configuration
-
-  require('./confg/passport')(passport)
-
-
-
+require("./confg/passport")(passport);
 
 //use Routes
-app.use('/api/users', users)
-app.use('/api/posts', posts)
-app.use('/api/profile', profile)
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
 
 const PORT = process.env.PORT || 5000;
 
